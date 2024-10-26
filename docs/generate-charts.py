1b030fe60f8dfc85e5e0d8bedf1f9da318cc544a
Andrey Davydov <andrey.davydov@jetbrains.com>
2024-10-26 12:15:56 +0200

    move string templates out of generate-charts.py

--- docs/generate-charts.py
+++ docs/generate-charts.py
@@ -92,123 +92,21 @@ def generate(projects, t):
 
     return (generated_canvases, generated_script)
 
-prolog = """<!DOCTYPE html>
 
-<html>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
-<body>
+with open('template.html', 'r') as f:
+    html_template = f.read()
 
-<style>
-  .report-title {
-    font-size: 3rem;
-    font-weight: 600;
-    word-break: break-word;
-    text-align: center;
-  }
-  .report-graphs {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    flex-wrap: wrap;
-    width: 100%;
-  }
-</style>
-"""
-
-epilog = """
-</body>
-</html>
-"""
-
-verticalLinePlugin = """const verticalLinePlugin = {
-  getLinePosition: function (chart, pointIndex) {
-      const meta = chart.getDatasetMeta(0); // first dataset is used to discover X coordinate of a point
-      const data = meta.data;
-      return data[pointIndex]._model.x;
-  },
-  renderVerticalLine: function (chartInstance, pointIndex) {
-      const lineLeftOffset = 0.5 * (this.getLinePosition(chartInstance, pointIndex - 1) + this.getLinePosition(chartInstance, pointIndex));
-      const scale = chartInstance.scales['y-axis-0'];
-      const context = chartInstance.chart.ctx;
-
-      // render vertical line
-      context.beginPath();
-      context.strokeStyle = '#ff0000';
-      context.moveTo(lineLeftOffset, scale.top);
-      context.lineTo(lineLeftOffset, scale.bottom);
-      context.stroke();
-  },
-
-  afterDatasetsDraw: function (chart, easing) {
-      if (chart.config.lineAtIndex) {
-          chart.config.lineAtIndex.forEach(pointIndex => this.renderVerticalLine(chart, pointIndex));
-      }
-  }
-};
-
-Chart.plugins.register(verticalLinePlugin);
-"""
-
-generate_chart_js_function = """
-function generate_chart(name, data) {
-    new Chart(name, {
-      type: "line",
-      data: {
-        labels: data['labels'],
-        datasets: [{
-          fill: false,
-          lineTension: 0,
-          backgroundColor: "rgba(0,0,255,1)",
-          borderColor: "rgba(0,0,0,0)",
-          data: data['times']
-        }]
-      },
-      options: {
-        legend: {display: false},
-        tooltips: {
-          callbacks: {
-            title: function(tooltipItem, data) {
-              index = tooltipItem[0]['index']
-              build = data['labels'][index]
-              return "TC build: " + build + "\\n" + "commit: " + commit[index];
-            }
-          }
-        },
-        scales: {
-            xAxes: [{
-              scaleLabel: {
-                display: true,
-                labelString: 'TC build'
-              }
-            }],
-            yAxes: [{
-              scaleLabel: {
-                display: true,
-                labelString: 'seconds'
-              },
-              ticks: data['ticks']
-            }],
-        }
-      },
-      lineAtIndex: data['change_points']
-    }
-  );
-}
-"""
+with open('template.js', 'r') as f:
+    js_template = f.read()
 
 for t in ["indexing", "inspect-code"]:
     generated_canvases, generated_script = generate(projects, t)
 
     with open(t + ".html", 'w') as f:
-        f.write(prolog)
-        f.write(generated_canvases)
-        f.write(f"<script src=\"{t}.js\"></script>\n")
-        f.write(epilog)
+        f.write(html_template.replace("PLACE YOUR SHIT HERE", generated_canvases + f"<script src=\"{t}.js\"></script>"))
 
     with open(t + ".js", 'w') as f:
-        f.write(verticalLinePlugin)
+        f.write(js_template)
         f.write("\nconst commit = " + str(commits) + ";\n\n")
-        f.write(generate_chart_js_function + "\n\n")
         f.write(generated_script)
 
---
+++ docs/template.js
@@ -0,0 +1,73 @@
+const verticalLinePlugin = {
+  getLinePosition: function (chart, pointIndex) {
+      const meta = chart.getDatasetMeta(0); // first dataset is used to discover X coordinate of a point
+      const data = meta.data;
+      return data[pointIndex]._model.x;
+  },
+  renderVerticalLine: function (chartInstance, pointIndex) {
+      const lineLeftOffset = 0.5 * (this.getLinePosition(chartInstance, pointIndex - 1) + this.getLinePosition(chartInstance, pointIndex));
+      const scale = chartInstance.scales['y-axis-0'];
+      const context = chartInstance.chart.ctx;
+
+      // render vertical line
+      context.beginPath();
+      context.strokeStyle = '#ff0000';
+      context.moveTo(lineLeftOffset, scale.top);
+      context.lineTo(lineLeftOffset, scale.bottom);
+      context.stroke();
+  },
+
+  afterDatasetsDraw: function (chart, easing) {
+      if (chart.config.lineAtIndex) {
+          chart.config.lineAtIndex.forEach(pointIndex => this.renderVerticalLine(chart, pointIndex));
+      }
+  }
+};
+
+Chart.plugins.register(verticalLinePlugin);
+
+function generate_chart(name, data) {
+    new Chart(name, {
+      type: "line",
+      data: {
+        labels: data['labels'],
+        datasets: [{
+          fill: false,
+          lineTension: 0,
+          backgroundColor: "rgba(0,0,255,1)",
+          borderColor: "rgba(0,0,0,0)",
+          data: data['times']
+        }]
+      },
+      options: {
+        legend: {display: false},
+        tooltips: {
+          callbacks: {
+            title: function(tooltipItem, data) {
+              index = tooltipItem[0]['index']
+              build = data['labels'][index]
+              return "TC build: " + build + "\n" + "commit: " + commit[index];
+            }
+          }
+        },
+        scales: {
+            xAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'TC build'
+              }
+            }],
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'seconds'
+              },
+              ticks: data['ticks']
+            }],
+        }
+      },
+      lineAtIndex: data['change_points']
+    }
+  );
+}
+
